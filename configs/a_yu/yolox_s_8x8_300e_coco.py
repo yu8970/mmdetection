110bb213fa812b06add50951e88d82f9919ea51b
ysc <ee2666@163.com>
2023-02-28 11:24:18 +0800

    update

--- configs/a_yu/yolox_s_8x8_300e_coco.py
+++ configs/a_yu/yolox_s_8x8_300e_coco.py
@@ -87,7 +87,7 @@ test_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=16,  # 单个 GPU 的 Batch size
+    samples_per_gpu=8,  # 单个 GPU 的 Batch size
     workers_per_gpu=8,   # 单个 GPU 分配的数据加载线程数
     persistent_workers=True,
     train=train_dataset,
