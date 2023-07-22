d5963af6b296b6b8cb1b3913827084ec634b7ceb
ysc <ee2666@163.com>
2023-07-22 21:33:37 +0800

    update

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/content/coco2017/'
+data_root = '/root/autodl-tmp/coco2017/'
 
 # Example to use different file client
 # Method 1: simply set the data root and let the file I/O module
